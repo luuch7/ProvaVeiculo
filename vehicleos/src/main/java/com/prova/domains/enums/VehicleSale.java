b9b1b04f074b2236c5380e949bd4dd051b7d4d60
Lucas Uvera <80430724+lucasuvera@users.noreply.github.com>
2024-09-10 20:45:38 -0300

    Enum Vehicle sale getter and setters

--- vehicleos/src/main/java/com/prova/domains/enums/VehicleSale.java
+++ vehicleos/src/main/java/com/prova/domains/enums/VehicleSale.java
@@ -2,4 +2,30 @@ package com.prova.domains.enums;
 
 public enum VehicleSale {
     
+    DONE(0,"ROLE_DONE"), INPROGRESS(1,"ROLE_INPROGRESS"), INSALE(2,"ROLE_INSALE");
+
+    private Integer id;
+    private String VehicleSale;
+    
+    private VehicleSale(Integer id, String vehicleSale) {
+        this.id = id;
+        VehicleSale = vehicleSale;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getVehicleSale() {
+        return VehicleSale;
+    }
+
+    public void setVehicleSale(String vehicleSale) {
+        VehicleSale = vehicleSale;
+    }
+
 }
