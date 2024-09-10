310a1ef66161f8f351d6e03e98497dcb0b12a8e9
Guilherme Vieira <63508067+luuch7@users.noreply.github.com>
2024-09-09 21:49:09 -0300

    Add files via upload

---
+++ vehicleos/src/main/java/com/prova/vehicleos/VehicleosApplication.java
@@ -0,0 +1,13 @@
+package com.prova.vehicleos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleosApplication.class, args);
+	}
+
+}
---
+++ vehicleos/src/test/java/com/prova/vehicleos/VehicleosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prova.vehicleos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
