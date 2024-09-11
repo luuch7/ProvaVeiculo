0404773fec4e8d445fde845337df4a4afb7ec244
Lucas Uvera <80430724+lucasuvera@users.noreply.github.com>
2024-09-11 19:14:57 -0300

    Enum VEhicleSale

--- vehicleos/src/main/java/com/prova/domains/enums/VehicleSale.java
+++ vehicleos/src/main/java/com/prova/domains/enums/VehicleSale.java
@@ -5,27 +5,42 @@ public enum VehicleSale {
     DONE(0,"ROLE_DONE"), INPROGRESS(1,"ROLE_INPROGRESS"), INSALE(2,"ROLE_INSALE");
 
     private Integer id;
-    private String VehicleSale;
+    private String vehicleSale;
     
+
     private VehicleSale(Integer id, String vehicleSale) {
         this.id = id;
-        VehicleSale = vehicleSale;
+        this.vehicleSale = vehicleSale;
     }
+
 
     public Integer getId() {
         return id;
     }
 
+
     public void setId(Integer id) {
         this.id = id;
     }
 
+
     public String getVehicleSale() {
-        return VehicleSale;
+        return vehicleSale;
     }
+
 
     public void setVehicleSale(String vehicleSale) {
-        VehicleSale = vehicleSale;
+        this.vehicleSale = vehicleSale;
     }
 
+
+    public static VehicleSale toEnum(Integer id){
+        if(id==null) return null;
+        for(VehicleSale x : VehicleSale.values()){
+            if(id.equals(x.getId())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Perfil Invalido");
+    }
 }
