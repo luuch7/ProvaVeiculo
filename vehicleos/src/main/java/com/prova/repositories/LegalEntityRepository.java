b6eaacfe9e8a11f6f9d0bfaa75a468b53c15a086
Lucas Uvera <80430724+lucasuvera@users.noreply.github.com>
2024-09-11 21:08:59 -0300

    comitar novamente

--- vehicleos/src/main/java/com/prova/repositories/LegalEntityRepository.java
+++ vehicleos/src/main/java/com/prova/repositories/LegalEntityRepository.java
@@ -1,6 +1,6 @@
 package com.prova.repositories;
 
-import org.hibernate.validator.constraints.UUID;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
