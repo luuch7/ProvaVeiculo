bb4dc9fb2217401b48fbf1af5b79e5b0842998c6
Lucas Uvera <80430724+lucasuvera@users.noreply.github.com>
2024-09-14 16:18:57 -0300

    DevConfig

--- vehicleos/src/main/java/com/prova/services/DevConfig.java
+++ vehicleos/src/main/java/com/prova/services/DevConfig.java
@@ -1,5 +1,29 @@
 package com.prova.services;
 
+import org.aspectj.internal.lang.annotation.ajcDeclareAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import jakarta.annotation.PostConstruct;
+
+
+@Configuration
+@Profile("dev")
 public class DevConfig {
     
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    @PostConstruct
+    public void initDB() {
+        if (value.equals("create")) {
+            this.dbService.initDB();
+        }
+    }
+
 }
