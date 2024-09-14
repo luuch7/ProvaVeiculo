2a10d76612c7ece15ba9082f0655d9ea5d12cc3a
Guilherme Vieira <63508067+luuch7@users.noreply.github.com>
2024-09-14 16:44:31 -0300

    TestDB

--- vehicleos/src/main/java/com/prova/config/TestConfig.java
+++ vehicleos/src/main/java/com/prova/config/TestConfig.java
@@ -1,10 +1,22 @@
 package com.prova.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.prova.services.DBService;
+
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
     
+    @Autowired
+    private DBService dbService;
+
+    @PostConstruct
+    public void initDB(){
+        this.dbService.initDB();
+    }
 }
