7ada0be9d096f5edfcc1f0042cb63f26186407bb
Lucas Uvera <80430724+lucasuvera@users.noreply.github.com>
2024-09-14 22:26:27 -0300

    ClientDTO

--- vehicleos/src/main/java/com/prova/domains/dtos/ClientDTO.java
+++ vehicleos/src/main/java/com/prova/domains/dtos/ClientDTO.java
@@ -1,5 +1,87 @@
 package com.prova.domains.dtos;
 
+import java.util.UUID;
+
 public class ClientDTO {
+    
+    protected UUID id;
+    protected String name;
+    protected String cpfCnpj;
+    protected String email;
+    protected String password;
+    protected String dateBirth;
+    protected String phoneNumber;
+    
+    public ClientDTO() {
+    }
+
+    public ClientDTO(UUID id, String name, String cpfCnpj, String email, String password, String dateBirth,
+            String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.cpfCnpj = cpfCnpj;
+        this.email = email;
+        this.password = password;
+        this.dateBirth = dateBirth;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(String dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
     
 }
